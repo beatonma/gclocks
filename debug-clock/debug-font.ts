e4c976fcac40b0855a8eb2babd7d6259f63552bd
Michael Beaton <beatonma@gmail.com>
2023-06-02 14:22:26 +0100

    Obsolete debug classes

--- debug-clock/debug-font.ts
+++
@@ -1,8 +0,0 @@
-import { BaseFont } from "../render";
-import { DebugGlyph } from "./debug-glyph";
-
-export class DebugFont extends BaseFont<DebugGlyph> {
-    isMonospace = true;
-
-    getGlyph = (index: number) => new DebugGlyph();
-}
--- debug-clock/debug-renderer.ts
+++
@@ -1,9 +0,0 @@
-import { BaseClockRenderer } from "../render";
-import { DebugFont } from "./debug-font";
-import { DebugGlyph } from "./debug-glyph";
-
-export class DebugRenderer extends BaseClockRenderer<DebugFont, DebugGlyph> {
-    buildFont(): DebugFont {
-        return new DebugFont();
-    }
-}
